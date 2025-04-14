e38b1f2e8608082e3a44411dc60172097d368607
Angela Tricarico <angelatricarico2000@gmail.com>
2025-04-14 15:49:56 +0200

    html/css/js profilo-prodotto-categoria-login

---
+++ resources/JS/caricaImmagine.js
@@ -0,0 +1,13 @@
+function previewImage(event) {
+    const input = event.target;
+    const preview = document.getElementById("preview-img");
+
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        preview.src = e.target.result;
+        preview.style.display = "block";
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
